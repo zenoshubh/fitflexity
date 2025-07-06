5305e9ae3f61e04f6ec2208d08f61070ea0801d5
Shubh Verma <shubhverma2003@gmail.com>
2025-07-07 00:11:28 +0530

    refactor(error): simplify error handling middleware and remove custom ApiError logic

--- server/src/middlewares/error.middleware.ts
+++ server/src/middlewares/error.middleware.ts
@@ -1,29 +1,16 @@
-import { ApiError } from "@/utils/ApiError";
-import type { Request, Response, NextFunction } from "express";
+import type { NextFunction, Request, Response } from "express";
 
-export const errorMiddleware = (
-    err: any,
-    req: Request,
-    res: Response,
-    next: NextFunction
-) => {
-    // If it's an instance of your custom ApiError
-    if (err instanceof ApiError) {
-        return res.status(err.statusCode).json({
-            success: err.success,
-            message: err.message,
-            errors: err.errors,
-            data: err.data,
-        });
-    }
 
-    // Else handle other unexpected errors
-    console.error("Unexpected Error:", err);
+const errorMiddleware = (err: any, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Internal Server Error";
 
-    return res.status(500).json({
+    res.status(statusCode).json({
         success: false,
-        message: "Internal Server Error",
-        errors: [],
-        data: null,
+        statusCode,
+        message,
+        errors: err.errors || []
     });
-};
+}
+
+export default errorMiddleware;

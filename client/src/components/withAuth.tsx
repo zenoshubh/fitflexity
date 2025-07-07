5b458b9802363643218d5f6cc956f89a2b329cd3
Shubh Verma <shubhverma2003@gmail.com>
2025-07-07 17:26:24 +0530

    feat(auth): add withAuth higher-order component for authentication handling

---
+++ client/src/components/withAuth.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useAuth } from "@/hooks/useAuth";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import type { FC } from "react";
+
+const withAuth = <P extends object>(WrappedComponent: FC<P>): FC<P> => {
+  const ComponentWithAuth: FC<P> = (props) => {
+    const { isAuthenticated, loading, user } = useAuth();
+    const router = useRouter();
+
+    useEffect(() => {
+      if (!loading && !isAuthenticated) {
+        router.replace("/login");
+      }
+
+      if (!user?.isProfileComplete && isAuthenticated) {
+        router.replace("/complete-profile");
+      }
+    }, [loading, isAuthenticated, router]);
+
+    if (!isAuthenticated) {
+      return null;
+    }
+
+    return <WrappedComponent {...props} />;
+  };
+
+  return ComponentWithAuth;
+};
+
+export default withAuth;

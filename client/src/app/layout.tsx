51b30858e223f91717b3c89d2e013a34a35ac9d9
Shubh Verma <shubhverma2003@gmail.com>
2025-07-08 11:59:19 +0530

    feat(layout): add Toaster component for notifications

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/AuthContext";
+import { Toaster } from "sonner";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <AuthProvider>{children}</AuthProvider>
+        <Toaster richColors position="bottom-right"/>
       </body>
     </html>
   );

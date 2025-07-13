dbfd16800358ad27009f57b9f92b7bb7d6adb50b
Shubh Verma <shubhverma2003@gmail.com>
2025-07-13 16:49:39 +0530

    feat(diet): add fetch, update, and delete routes for diet plans

--- server/src/routes/diet.routes.ts
+++ server/src/routes/diet.routes.ts
@@ -1,10 +1,14 @@
-import { chatDietPlan, generateDietPlan } from "@/controllers/diet.controller";
+import { chatDietPlan, updateDietPlan, deleteDietPlan, fetchDietPlan, generateDietPlan } from "@/controllers/diet.controller";
 import { verifyJWT } from "@/middlewares/auth.middleware";
 import { Router } from "express";
 
 const router = Router()
 
 router.route("/generate-diet-plan").post(verifyJWT, generateDietPlan)
-router.route("/chat-diet-plan").post(verifyJWT , chatDietPlan)
+router.route("/chat-diet-plan").post(verifyJWT, chatDietPlan)
+router.route("/get-diet-plan").get(verifyJWT, fetchDietPlan)
+router.route("/update-diet-plan").put(verifyJWT, updateDietPlan)
+router.route("/delete-diet-plan").delete(verifyJWT, deleteDietPlan)
+
 
 export default router;

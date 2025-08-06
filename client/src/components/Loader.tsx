b40962adefc169bcca04f7d884b92ebc0b02fa91
Shubh Verma <shubhverma2003@gmail.com>
2025-08-06 22:31:40 +0530

    ui: update Loader component border color from blue to orange

--- client/src/components/Loader.tsx
+++ client/src/components/Loader.tsx
@@ -1,7 +1,7 @@
 // components/ui/Loader.tsx
 const Loader = () => (
   <div className="min-h-screen flex items-center justify-center">
-    <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-blue-500"></div>
+    <div className="animate-spin rounded-full h-10 w-10 border-t-2 border-b-2 border-orange-500"></div>
   </div>
 );
 
